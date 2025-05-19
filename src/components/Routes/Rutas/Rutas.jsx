9c22d8352b28fa0bbdf9e6ec4a0d3f620126df34
Alvaro Perez <juan@gmail.com>
2025-05-19 14:13:51 -0500

    se crea cambio final

--- src/components/Routes/Rutas/Rutas.jsx
+++ src/components/Routes/Rutas/Rutas.jsx
@@ -5,12 +5,16 @@ import {Historia} from "../../pages/Historia/Historia"
 import {Introduccion} from "../../pages/Introduccion/Introduccion"
 import {Experiencia} from "../../pages/Experiencia/Experiencia"
 
+import { Menu } from '../../common/Menu/Menu'
+
 export function Rutas(){
     return(
 
         <>
+            <Menu></Menu>
 
             <Routes>
+                <Route path='/' element={<Introduccion/>} ></Route>
                 <Route  path='/inicio' element={<Home/>} ></Route>
                 <Route path='/proyectos' element={<Historia/>} ></Route>
                 <Route path='/juanjosegallego' element={<Introduccion/>} ></Route>
